6350293ae7df3bcbc5feb5dbeeea3e6c5ce44574
Andrew <32229265+aselee@users.noreply.github.com>
2018-07-06 17:21:46 -0500

    map not responsive

--- music-app/src/components/Map.js
+++ music-app/src/components/Map.js
@@ -1,43 +1,91 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 
-export class Map extends React.Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-    componentDidUpdate(prevProps, prevState){
-        if (prevProps.google !== this.props.google) {
-            this.loadMap();
-        }
-    }
-    loadMap(){
-        if (this.props && this.props.google) {
-            const{google} = this.props;
-            const maps = google.maps;
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
-            const mapRef = this.refs.map;
-            const node = ReactDOM.findDOMNode(mapRef);
+function Map(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
 
-            let zoom = 14;
-            let lat = 37.774929;
-            let lng = -122.419416
-            const center = new maps.LatLng(lat,lng);
-            const mapConfig = Object.assign({}, {
-                center: center,
-                zoom: zoom
-            })
-            this.map = new maps.Map(node, mapConfig);
-        }
-    }
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+ 
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
 
-    render() {
-      return (
-        <div ref='map'>
-          Loading map...
-        </div>
-      )
-    }
-  }
+Map.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-  export default Map;
+export default withStyles(styles)(SimpleCard);
+
+
+// export class Map extends React.Component {
+
+    // componentDidUpdate(prevProps, prevState){
+    //     if (prevProps.google !== this.props.google) {
+    //         this.loadMap();
+    //     }
+    // }
+    // loadMap(){
+    //     if (this.props && this.props.google) {
+    //         const{google} = this.props;
+    //         const maps = google.maps;
+
+    //         const mapRef = this.refs.map;
+    //         const node = ReactDOM.findDOMNode(mapRef);
+
+    //         let zoom = 14;
+    //         let lat = 37.774929;
+    //         let lng = -122.419416
+    //         const center = new maps.LatLng(lat,lng);
+    //         const mapConfig = Object.assign({}, {
+    //             center: center,
+    //             zoom: zoom
+    //         })
+    //         this.map = new maps.Map(node, mapConfig);
+    //     }
+    // }
+
+//     render() {
+//       return (
+//         <div ref='map'>
+//           Loading map...
+//         </div>
+//       )
+//     }
+//   }
+
+//   export default Map;
 
 // import React from 'react';
 // import {
