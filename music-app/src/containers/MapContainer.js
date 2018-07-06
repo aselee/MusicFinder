a52c1d33b72f537566490df0615566d7d1086937
Andrew <32229265+aselee@users.noreply.github.com>
2018-07-06 17:23:28 -0500

    GMAP

--- music-app/src/containers/MapContainer.js
+++ music-app/src/containers/MapContainer.js
@@ -1,21 +1,31 @@
-import react from 'react';
+import { connect } from 'react-redux';
+import Map from '../components/Map';
 
-export class Container extends React.Component {
-    render() {
-        const style = {
-            width: '100vw',
-            height: '100vh'
-          }
+function mapStateToProps(state) {
+    // console.log(state)
+    return {
+        location: state.locations
+    };
+}
 
-      return(
-          <div>
-            <Map google={this.props.google} />
-          </div>
-      )
-    }
-  }
+export default connect(mapStateToProps)(Map);
+
+// export class Container extends React.Component {
+//     render() {
+//         const style = {
+//             width: '100vw',
+//             height: '100vh'
+//           }
+
+//       return(
+//           <div>
+//             <Map google={this.props.google} />
+//           </div>
+//       )
+//     }
+//   }
   
-  export default GoogleApiComponent({
-    apiKey: AIzaSyA5EQG52zu9Crfxfs7aMxkMLQvuHa_ntO4
-  })(Container)
+//   export default GoogleApiComponent({
+//     apiKey: AIzaSyA5EQG52zu9Crfxfs7aMxkMLQvuHa_ntO4
+//   })(Container)
 
